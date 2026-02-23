9b59f4c37c8780110cbcc24b530d06870bcc57e5
Ksanjiwani <sanjiwanikandekar6776@gmail.com>
2026-02-23 21:30:00 +0530

    Hello Tester Initial Commit

---
+++ src/Basic_Java_Concept/HelloTester.java
@@ -0,0 +1,11 @@
+//Statement:- write a java program to print HelloTester.
+
+package Basic_Java_Concept;
+
+public class HelloTester {
+	public static void main(String args[])
+	{
+		System.out.println("Hello Tester...!");
+	}
+
+}
