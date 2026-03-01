c27dd4b89a8cf3acffbd019152152328851f6a2a
Ksanjiwani <sanjiwanikandekar6776@gmail.com>
2026-03-01 20:40:19 +0530

    largest number

---
+++ src/Common_Program/Largest_NO.java
@@ -0,0 +1,24 @@
+package Common_Program;
+
+public class Largest_NO {
+	public static void main(String args[])
+	{
+		int no1=76, no2=19, no3=90;
+		if(no1 > no2 && no1 > no3)
+		{
+			System.out.println("no1 is greater");
+		}
+		else if(no2 > no1 && no2 > no3)
+		{
+			System.out.println("no2 is greater");
+		}
+		else
+		{
+			System.out.println("no3 is greater");
+		}
+		
+	}
+	
+	
+
+}
