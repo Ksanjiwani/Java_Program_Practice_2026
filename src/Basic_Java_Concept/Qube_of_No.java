579cb2431af884d8cfd3302186775d2ec6faa553
Ksanjiwani <sanjiwanikandekar6776@gmail.com>
2026-03-04 21:51:55 +0530

    square and qube

---
+++ src/Basic_Java_Concept/Qube_of_No.java
@@ -0,0 +1,11 @@
+package Basic_Java_Concept;
+
+public class Qube_of_No {
+	public static void main(String args[])
+	{
+		int no = 12,cube;
+		cube = no * no * no ;
+		System.out.println(cube);
+	}
+
+}
---
+++ src/Common_Program/Square_Of_No.java
@@ -0,0 +1,12 @@
+package Common_Program;
+
+public class Square_Of_No {
+	public static void main(String args[])
+	{
+		int no = 10,square;
+		square = no * no;
+		System.out.println(square);
+		
+	}
+
+}
