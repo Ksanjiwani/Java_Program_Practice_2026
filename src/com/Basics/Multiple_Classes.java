16e837fdc467af7a993223d7ed41f0009605bfc2
Ksanjiwani <sanjiwanikandekar6776@gmail.com>
2026-03-13 08:00:19 +0530

    Multiple_classes

---
+++ src/com/Basics/Multiple_Classes.java
@@ -0,0 +1,25 @@
+package com.Basics;
+
+public class Multiple_Classes {
+	public static void main(String args[])
+	{
+		System.out.println("First Program");
+	}
+
+}
+
+class Second
+{
+	public static void main(String args [])
+	{
+		System.out.println("Second Program");
+	}
+}
+
+class Third
+{
+	public static void main(String args[])
+	{
+		System.out.println("Third Program");
+	}
+}
